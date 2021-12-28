c6e1bc06972be334ce7a924977d3db7249c8ec91
mohamad hosein <mmdtrni@gmail.com>
2021-12-28 17:43:51 +0330

    general structure

---
+++ main.py
@@ -0,0 +1,31 @@
+import auth
+import models
+
+print("Custom Account Banking Management System")
+print("Created By MohamadHosein Toorani & Maede Naderloo\n")
+print("-----Welcome To The Main Menu-----\n")
+
+print("1.Create new account")
+print("2.Update information of existing account")
+print("3.For transactions")
+print("4.Check the details of existing account")
+print("5.Removing existing account")
+print("6.View customer's list")
+print("7.Exit\n\n")
+
+entity = input("Enter your choice:\n")
+
+if entity == 1:
+    pass
+elif entity == 2:
+    pass
+elif entity == 3:
+    pass
+elif entity == 4:
+    pass
+elif entity == 5:
+    pass
+elif entity == 6:
+    pass
+elif entity == 7:
+    pass
