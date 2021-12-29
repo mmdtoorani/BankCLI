f81428b00a7c3c304edc011e3323becddbd23e47
mohamad hosein <mmdtrni@gmail.com>
2021-12-29 06:25:15 +0330

    just for cleaning main

---
+++ envvar.py
@@ -0,0 +1,16 @@
+TITLE = "Custom Account Banking Management System"
+
+LICENCE = "Created By MohamadHosein Toorani & Maede Naderloo"
+
+OPTIONS = """
+    1.Create new account
+    2.Update information of existing account
+    3.For transactions
+    4.Check the details of existing account
+    5.Removing existing account
+    6.View customer's list
+    7.Exit
+"""
+
+
+
