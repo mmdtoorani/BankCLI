fc3594a1886afe74bb6bd3159f84263eaa6df589
mohamad hosein <mmdtrni@gmail.com>
2021-12-31 04:52:58 +0330

    user authentication Class base

--- auth.py
+++ auth.py
@@ -2,35 +2,6 @@ from models import User, Account
 from utils import make_hash, generate_token
 
 
-def registration(username: str, password: str):
-    """Creation a user and then his/her account"""
-
-    hashed_pass = make_hash(password)
-    user = User(username=username, password=hashed_pass)
-    user.save()
-
-    account = Account(
-        user=user,
-        address=generate_token(),
-    )
-    account.save()
-
-
-def is_user_exist(username: str):
-    if User.objects(username=username):
-        return True
-
-    return False
-
-
-def is_user_valid(username, password):
-    """Check for correct password entry by the user"""
-    if User.objects.get(username=username).password == make_hash(password):
-        return True
-
-    return False
-
-
 def is_name_valid(name: str) -> bool:
     """
     Check for first name or last name validation.
@@ -38,31 +9,133 @@ def is_name_valid(name: str) -> bool:
     it must be only uppercase and lowercase letters, otherwise it's invalid
 
     """
-    alphabet_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alphabet_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    alphabet_lower = "abcdefghijklmnopqrstuvwxyz"
+    alphabet_lowercase = "abcdefghijklmnopqrstuvwxyz"
 
     chars = [i for i in name]
-    result_list = [True if i in alphabet_lower or i in alphabet_upper else False for i in chars]
+    result_list = [True if i in alphabet_lowercase or i in alphabet_uppercase else False for i in chars]
 
     if all(result_list):  # This will be True, if all the indexes in the result_list are True
         return True
-
     return False
 
 
-def user_authenticated(username, password):
-    if is_user_exist(username):
-        if is_user_valid(username, password):
+def users_list():
+    return User.objects.all()
+
+
+class UserAuthentication:
+    msg = None
+
+    def __init__(self, username, password, age=None, first_name=None, last_name=None):
+        self.username = username
+        self.password = password
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    @property
+    def is_user_exist(self):
+        if User.objects(username=self.username):
             return True
+        return False
+
+    def registration(self):
+        """Creation a user and then his/her account"""
+
+        if not self.is_user_exist:
+            if not len(self.password) < 6:
+                hashed_pass = make_hash(self.password)
+                user = User(
+                    username=self.username,
+                    password=hashed_pass
+                )
+                user.save()
+
+                account = Account(
+                    user=user,
+                    address=generate_token(),
+                )
+                account.save()
+                msg = "You registered successfully!"
+            else:
+                msg = "Your password is too short!"
         else:
-            msg = "Invalid password!"
-    else:
-        msg = "You have not registered!"
+            msg = "ERROR: User already exists...!"
 
-    return False, msg
+        print(msg)
 
+    @property
+    def is_user_valid(self):
+        """Check for correct password entry by the user"""
+        if User.objects.get(username=self.username).password == make_hash(self.password):
+            return True
+        return False
 
-def collect_user_info(username, first_name, last_name, age):
-    user = User.objects.get(username=username)
-    user.update(first_name=first_name, last_name=last_name, age=age)
+    @property
+    def is_authenticated(self):
+        if self.is_user_exist:
+            if self.is_user_valid:
+                return True
+            else:
+                print("Invalid password!")
+        else:
+            print("You have not registered!")
+
+        return False
+
+    def collect_user_info(self):
+        if is_name_valid(self.first_name) and is_name_valid(self.last_name):
+            try:
+                int(self.age)
+                msg = ""
+            except ValueError:
+                msg = "Invalid input! Try again"
+
+            if msg is not "Invalid input! Try again":
+                user = User.objects.get(username=self.username)
+                user.update(
+                    first_name=self.first_name,
+                    last_name=self.last_name,
+                    age=self.age
+                )
+                msg = 'your account has been updated!'
+        else:
+            msg = "Invalid input! Try again"
+
+        print(msg)
+
+    def show_info(self):
+        if self.is_authenticated:
+            user_id = User.objects.get(username=self.username).id
+            account = Account.objects.get(user=user_id)
+            print(f"\nusername: {self.username}\n")
+
+            if self.first_name:
+                print(f"First name: {self.first_name}\n")
+            else:
+                print("First name: Not set\n")
+
+            if self.last_name:
+                print(f"Last name: {self.last_name}\n")
+            else:
+                print("Last name: Not set\n")
+
+            if self.age:
+                print(f"Age: {self.age}\n")
+            else:
+                print("Age: Not set\n")
+
+            print(
+                f"finance: {account.finance}\n\n"
+                f"number of transactions: {account.number_of_transactions}\n\n"
+                f"Your Token address: {account.address}\n\n"
+                f"date of last transaction: {account.date_of_last_transaction}"
+            )
+
+    def remove(self):
+        if self.is_authenticated:
+            user = User.objects.get(username=self.username)
+            user.delete()
+            print("User has been removed successfully!")
--- main.py
+++ main.py
@@ -12,48 +12,19 @@ choice = int(input("Enter your choice:\n"))
 if choice == 1:
     username = input("Enter your username:")
     password = input("Enter your password:")
-
-    if not is_user_exist(username):
-        if not len(password) < 6:
-            registration(username, password)
-            msg = "You registered successfully!"
-        else:
-            msg = "Your password is too short!"
-    else:
-        msg = "ERROR: User already exists...!"
-
-    print(msg)
-
+    user = UserAuthentication(username, password)
+    user.registration()
 
 elif choice == 2:
     username = input("Enter your username:")
     password = input("Enter your password:")
+    user = UserAuthentication(username, password)
 
-    if is_user_exist(username):
-        if is_user_valid(username, password):
-            first_name = input("enter your first name: ")
-            last_name = input("enter your last name: ")
-            age = input("enter your age: ")
-            if is_name_valid(first_name) and is_name_valid(last_name):
-                try:
-                    int(age)
-                    msg = ""
-                except ValueError:
-                    msg = "Invalid input! Try again"
-
-                if msg is not "Invalid input! Try again":
-                    collect_user_info(username, first_name, last_name, age)
-                    msg = 'your account has been updated!'
-
-            else:
-                msg = "Invalid input! Try again"
-        else:
-            msg = "Invalid password!"
-    else:
-        msg = "You have not registered!"
-
-    print(msg)
-
+    if user.is_authenticated:
+        user.first_name = input("enter your first name: ")
+        user.last_name = input("enter your last name: ")
+        user.age = input("enter your age: ")
+        user.collect_user_info()
 
 elif choice == 3:
     pass
@@ -61,25 +32,20 @@ elif choice == 3:
 elif choice == 4:
     username = input("Enter your username:")
     password = input("Enter your password:")
-
-    if user_authenticated(username, password):
-        first_name = User.objects.get(username=username).first_name
-        last_name = User.objects.get(username=username).last_name
-        age = User.objects.get(username=username).age
-
-        print(f"username: {username}\nfirst name: {first_name}\nlast name: {last_name}\nage: {age}")
-    else:
-        print(user_authenticated(username, password)[1])  # it prints the message
-
+    user = UserAuthentication(username, password)
+    user.show_info()
 
 elif choice == 5:
-    pass
+    username = input("Enter your username:")
+    password = input("Enter your password:")
+    user = UserAuthentication(username, password)
+    user.remove()
 
 elif choice == 6:
-    pass
+    print(users_list())
 
 elif choice == 7:
-    pass
+    exit()
 
 else:
     msg = "Invalid input! Try again later"
