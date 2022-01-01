beb53ffdeafd1a81cee1ae5e0334e720c6f55849
mohamad hosein <mmdtrni@gmail.com>
2022-01-02 01:08:10 +0330

    combine auth and utils

--- utils.py
+++ utils.py
@@ -1,14 +1,1 @@
-import hashlib
-import secrets
 
-
-def make_hash(password):
-    """Convert user's password to hash using md5 algorithm."""
-
-    hash_obj = hashlib.md5(str.encode(password))  # <md5 hash object>
-    hexdigest = hash_obj.hexdigest()
-    return hexdigest
-
-
-def generate_token():
-    return secrets.token_hex(16)
